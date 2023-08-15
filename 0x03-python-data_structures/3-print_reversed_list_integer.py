529ee4ac78d5abc5de80a6f670e96feafa085515
BADR <benabboudbadr@gmail.com>
2023-08-15 16:23:38 +0000

    taskkk

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-st_integer(my_list=[]):
+def print_reversed_list_integer(my_list=[]):
     if my_list is not None:
         for i in range(len(my_list)):
             print("{:d}".format(my_list[::-1][i]))
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-_c(my_string):
+def no_c(my_string):
     new_string = ""
     for char in my_string:
         if char != 'c' and char != 'C':
