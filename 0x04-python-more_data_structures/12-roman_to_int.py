f353d55b7a78d66a54640b68daf086b2bd173309
BADROO <benabboudbadr@gmail.com>
2023-08-17 02:24:14 -0700

    task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    sum = 0
-    index = 0
-    if roman_string is not None:
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                 'C': 100, 'D': 500, 'M': 1000}
-        listrom = list(roman.keys())
-        length = len(roman_string)
-        for char in roman_string:
-            # print("++ "+char)
-            if char in roman:
-                # print("+ "+char, roman[char])
-                if char in 'IXC':
-                    if index < length-1:
-                        if listrom[index+1] in 'VXLCDM':
-                            sum = sum + (roman[listrom[index+1]]-roman[char])
-                sum += roman[char]
-            index += 1
-    return sum
-
-
-"""
-check if current charachter index is equal to lenght
-"I" can be subtracted from "V" and "X",
-"X" can be subtracted from "L" and "C",
-"C" can be subtracted from "D" and "M".
-However, "V", "L", and "D" cannot be used to represent subtraction.
-"""
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    decs = [roman_dict[x] for x in roman_string]
+    output = 0
+    for i in range(len(decs)):
+        output += decs[i]
+        if decs[i - 1] < decs[i] and i != 0:
+            output -= (decs[i - 1] + decs[i - 1])
+    return output
