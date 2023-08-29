48a7db1928191730d9af7a43606bb1f5026f5ac9
BADR <benabboudbadr@gmail.com>
2023-08-29 02:52:49 -0700

    task

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    counter = 0
+def safe_print_integer(value):
     try:
-        for ele in my_list:
-            if (counter >= x):
-                break
-            print("{}".format(ele), end="")
-            counter += 1
-        print("".format())
-        return counter
-    except ValueError:
-        print("ValueError error")
+        print("{:d}".format(value))
+    except(ValueError, TypeError):
+        return False
+    else:
+        return True
