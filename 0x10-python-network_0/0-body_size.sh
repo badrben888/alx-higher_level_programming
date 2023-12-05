0bfc2a9f7fe4a0a44c3144b7f48bccd9c915dc67
BADR <benabboudbadr@gmail.com>
2023-12-05 03:41:41 -0800

    tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a request to an URL with curl, and displays the size of the body of the response
-curl -s "$1" | wc -
+# Curl body size
+curl -sw '%{size_download}\n' -o /dev/null "$1"
